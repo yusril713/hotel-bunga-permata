b051c62019358cfdd646731a736c97fc77a4f173
Yusril Ihza <yusril@jakpat.net>
2022-07-16 02:15:06 +0700

    remove link unused social media

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -217,8 +217,6 @@
                         <div class="d-flex pt-2">
                             <a class="btn btn-outline-light btn-social" href="https://www.instagram.com/hotel_bunga_permata" target="_blank"><i class="fab fa-instagram"></i></a>
                             <a class="btn btn-outline-light btn-social" href="https://www.facebook.com/HOTELBUNGAPERMATA" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                 </div>
